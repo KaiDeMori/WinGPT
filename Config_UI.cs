9cd80bd0195b980e3593db7481ce26eba6cb2e35
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2023-08-26 01:58:48 +0200

    auto-scoll configurable using categories in UIable

--- Config_UI.cs
+++ Config_UI.cs
@@ -1,10 +1,11 @@
-﻿namespace WinGPT; 
+﻿namespace WinGPT;
 
 public partial class Config_UI : Form
 {
-   public Config_UI(Config_UIable config) {
-      InitializeComponent();
-      config_ui_propertyGrid.SelectedObject = config;
-   }
+    public Config_UI(Config_UIable config)
+    {
+        InitializeComponent();
+        config_ui_propertyGrid.SelectedObject = config;
+    }
 
 }
--- Config_UIable.cs
+++ Config_UIable.cs
@@ -6,13 +6,6 @@ public class Config_UIable {
    public int? Max_Tokens { get; set; }
 
    /// <summary>
-   /// This controls the auto-scrolling of the markf278down text-box.
-   /// </summary>
-   [DisplayName("Auto-Scroll markf278down")]
-   [Description("This controls the auto-scrolling of the markf278down text-box.")]
-   public bool Auto_Scroll { get; set; } = true;
-
-   /// <summary>
    /// This controls the function that enables saving via prompt.
    /// </summary>
    [DisplayName("Use Save via Prompt Function")]
@@ -30,12 +23,22 @@ public class Config_UIable {
    [Description("This controls the font of the prompt text-box.")]
    public Font Prompt_Font { get; set; } = new Font("Arial", 12);
 
-   [DisplayName("Response Font")]
-   [Description("This controls the font of the raw markf278down text-box.")]
+   [Category("markf278down text-box")]
+   [DisplayName("Font")]
+   [Description("This controls the font of the raw markf278down response text-box.")]
    public Font markf278down_Font { get; set; } = new Font("Arial", 12);
 
    // the corrected version would be
-   [DisplayName("markf278down text-box readonly")]
-   [Description("This controls the readonly property of the markf278down text-box.")]
+   [Category("markf278down text-box")]
+   [DisplayName("Readonly")]
+   [Description("If true, the response text-box is read-only.")]
    public bool markf278down_readonly { get; set; } = true;
+
+   /// <summary>
+   /// This controls the auto-scrolling of the markf278down text-box.
+   /// </summary>
+   [Category("markf278down text-box")]
+   [DisplayName("Use Auto-Scroll")]
+   [Description("If true, the response text-box is scrolled to the last response. ")]
+   public bool Auto_Scroll { get; set; } = true;
 }
