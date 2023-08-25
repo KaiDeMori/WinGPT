1df51226b87fa8ce4d49bb690fde9fbcc495a671
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2023-08-25 02:16:25 +0200

    dark magic. custom!

---
+++ CustomAction_Uninstall_ConfigDeletion.cs
@@ -0,0 +1,13 @@
+﻿namespace WinGPT;
+
+public partial class CustomAction_Uninstall_ConfigDeletion : Form
+{
+    public CustomAction_Uninstall_ConfigDeletion()
+    {
+        InitializeComponent();
+        this.AcceptButton = keep_button;
+
+        this.delete_button.DialogResult = DialogResult.Yes;
+        this.keep_button.DialogResult = DialogResult.No;
+    }
+}
---
+++ _PUBLISH_ALL_.bat
@@ -0,0 +1,14 @@
+@echo off
+dotnet publish -c Release -p:PublishProfile=selfsigned_ClickOnce
+rem dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o ReleaseFolderName -c Release /p:AssemblyVersion=1.2.3.4 /p:Version=1.2.3.4-product-version
+dotnet publish -c Release -p:PublishProfile=LocalFolder
+dotnet publish -c Release -p:PublishProfile=FolderProfile
+
+
+cd ../WinGPT_Setup
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" WinGPT_Setup.vdproj /Build Release
+cd ../WinGPT
+
+echo Done!
+
+rem pause
