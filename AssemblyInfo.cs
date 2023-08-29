c217af778e16af73ca45c28cfeeb6b3a8e95b2a3
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2023-08-29 18:59:13 +0200

    better way to zip specific publish profiles

--- AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// AssemblyInfo.cs
-
-using System.Reflection;
-
-[assembly: AssemblyVersion("0.1.12.0")]
-[assembly: AssemblyFileVersion("0.1.12.0")]
--- _PUBLISH_ALL_.bat
+++ _PUBLISH_ALL_.bat
@@ -3,6 +3,8 @@ dotnet publish -c Release -p:PublishProfile=selfsigned_ClickOnce
 rem dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o ReleaseFolderName -c Release /p:AssemblyVersion=1.2.3.4 /p:Version=1.2.3.4-product-version
 dotnet publish -c Release -p:PublishProfile=LocalFolder
 dotnet publish -c Release -p:PublishProfile=FolderProfile
+dotnet publish -c Release -p:PublishProfile=x64
+dotnet publish -c Release -p:PublishProfile=x86
 
 
 cd ../WinGPT_Setup
