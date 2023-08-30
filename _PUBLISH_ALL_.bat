ee5e14617f3d930013a60e3ad41cfae1dd75c6fa
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2023-08-30 18:45:01 +0200

    check if associated file exists

--- _PUBLISH_ALL_.bat
+++ _PUBLISH_ALL_.bat
@@ -1,6 +1,10 @@
 @echo off
-dotnet publish -c Release -p:PublishProfile=selfsigned_ClickOnce
+rem dotnet publish -c Release -p:PublishProfile=selfsigned_ClickOnce
 rem dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o ReleaseFolderName -c Release /p:AssemblyVersion=1.2.3.4 /p:Version=1.2.3.4-product-version
+
+rem "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+msbuild -t:Publish -p:Configuration=Release -p:PublishProfile=selfsigned_ClickOnce
+
 dotnet publish -c Release -p:PublishProfile=LocalFolder
 dotnet publish -c Release -p:PublishProfile=FolderProfile
 dotnet publish -c Release -p:PublishProfile=x64
