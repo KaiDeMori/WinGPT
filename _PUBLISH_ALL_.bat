16b4e2457d1273a99fce9cf80423ffdee9a137e8
KaiDeMori <Kai.De.Mori@peopleoftheprompt.org>
2023-11-26 06:24:11 +0100

    building is hard

--- _PUBLISH_ALL_.bat
+++ _PUBLISH_ALL_.bat
@@ -1,14 +1,16 @@
 @echo off
-rem dotnet publish -c Release -p:PublishProfile=selfsigned_ClickOnce
 rem dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -o ReleaseFolderName -c Release /p:AssemblyVersion=1.2.3.4 /p:Version=1.2.3.4-product-version
 
 rem "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-msbuild -t:Publish -p:Configuration=Release -p:PublishProfile=selfsigned_ClickOnce
 
-dotnet publish -c Release -p:PublishProfile=LocalFolder
-dotnet publish -c Release -p:PublishProfile=FolderProfile
-dotnet publish -c Release -p:PublishProfile=x64
-dotnet publish -c Release -p:PublishProfile=x86
+rem msbuild WinGPT.csproj -t:Publish -p:Configuration=Release -p:PublishProfile=selfsigned_ClickOnce
+rem "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" WinGPT.csproj -t:Publish -p:Configuration=Release -p:PublishProfile=selfsigned_ClickOnce
+rem dotnet publish WinGPT.csproj -c Release -p:PublishProfile=selfsigned_ClickOnce
+
+dotnet publish WinGPT.csproj -c Release -p:PublishProfile=LocalFolder
+dotnet publish WinGPT.csproj -c Release -p:PublishProfile=FolderProfile
+dotnet publish WinGPT.csproj -c Release -p:PublishProfile=x64
+dotnet publish WinGPT.csproj -c Release -p:PublishProfile=x86
 
 
 cd ../WinGPT_Setup
